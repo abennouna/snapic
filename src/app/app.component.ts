fc973dc25f0759d10819c00d80eb1ea6a81cf45c
Abdelaziz <abennouna@gmail.com>
2020-02-19 13:50:22 +0100

    chore(): cutomize & upgrade to Angular 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,24 @@
 import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins } from '@capacitor/core';
+const { SplashScreen } = Plugins;
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor() {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+    /* To make sure we provide the fastest app loading experience 
+       for our users, hide the splash screen automatically 
+       when the app is ready to be used:
+        
+        https://capacitor.ionicframework.com/docs/apis/splash-screen#hiding-the-splash-screen
+    */
+    SplashScreen.hide();
   }
 }
